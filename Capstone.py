5af3125039984fe1a6a97251e9bd6f8c6cfd8129
saadman-Bin-Jahan <saadman.nursery.21@gmail.com>
2026-05-16 22:34:38 +0600

    Add guessing game functionality in Capstone.py
    Implemented a guessing game with score tracking and player prompts.

---
+++ Capstone.py
@@ -0,0 +1,24 @@
+import random
+attempts_list = []
+def show_score():
+    if len(attempts_list) <= 0:
+        print("There are currently no high scores, It's yours for the taking!")
+    else:
+        print("The current high score is {} attempts".format(min(attempts_list)))
+        def start_game():10))
+        
+        guesses!")
+        player
+        wanna_play
+        like to play the guessing game
+        show_score()
+        while wanna_play.lower() == "yes":
+            try:
+                guess input("Pick a number between 1 and 10")
+                
+                raise ValueError("Please guess a number within the given range.")
+                print( It took you {} attempts(format)(attempts))
+                play_again = input("would you like to play again? Enter YES/NO")
+                attempts = 0
+                show_score{}
+                
