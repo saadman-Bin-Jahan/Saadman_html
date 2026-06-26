cf1e27e105d4019b2ee6e0405214ae0697687603
saadman-Bin-Jahan <saadman.nursery.21@gmail.com>
2026-06-26 18:30:59 +0600

    Update Power Calculator.py

---
+++ Power Calculator.py
@@ -0,0 +1,8 @@
+# Power Calculator
+
+base = float(input("Enter the base number: "))
+exponent = int(input("Enter the exponent: "))
+
+result = base ** exponent
+
+print(f"{base} raised to the power of {exponent} is {result}")
