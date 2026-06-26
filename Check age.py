a14d0adb6d86aca24899b5e4602decfac56b102d
saadman-Bin-Jahan <saadman.nursery.21@gmail.com>
2026-06-26 18:25:56 +0600

    Update Check age.py

---
+++ Check age.py
@@ -0,0 +1,8 @@
+# Age Eligibility Checker
+
+age = int(input("Enter your age: "))
+
+if age > 20 or age < 10:
+    print("You are ineligible for Raj Sir's classes.")
+else:
+    print("You are eligible for Raj Sir's classes.")
