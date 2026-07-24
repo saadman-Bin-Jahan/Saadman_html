72df3ee736354b9bc59e5fb00a149e6f633309f9
saadman-Bin-Jahan <saadman.nursery.21@gmail.com>
2026-07-24 22:25:16 +0600

    Number game.py

---
+++ Number Game.py
@@ -0,0 +1,8 @@
+import random
+playing = True
+number = str(random , randint(0, 9))
+
+print("I will generate a number between 0 and 9, and you have to guess the number one digit at a time.")
+print("The game ends when you get 1 hero!")
+while playing:
+  guess = input("Give me your best guess! ")
