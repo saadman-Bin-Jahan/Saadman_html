ad20445f458fefca46cfffa0816f16497bfce919
saadman-Bin-Jahan <saadman.nursery.21@gmail.com>
2026-09-06 16:17:33 +0600

    Update Library System.py

---
+++ Library System.py
@@ -0,0 +1,12 @@
+class Book:
+    def __init__(self, title, author):
+        pass
+    def borrow(self):
+        pass
+    def return_book(self):
+        pass
+    def __str__(self):
+        pass
+print("=" * 42)
+print("          LIBRARY SYSTEM")
+print("=" * 42)
